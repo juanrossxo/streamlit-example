cf76c0bfe2c3778b5c1d357a5b5ddf2dfaec3338
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-01-10 12:36:09 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import cv2
 
 """
 # Welcome to Streamlit!
@@ -36,3 +37,22 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+    
+    # Drawing Shapes
+ 
+def ImageProcessing():
+    image = np.zeros((512, 512, 3), np.uint8)
+ 
+    cv2.line(image, (20,200), (200,20), (0,0,255),5)
+    cv2.rectangle(image, (200,60), (20,200), (255,0,0), 3)
+    cv2.circle(image, (80,80), 50, (0,255,0), 4)
+ 
+    mytext = "Hello World"
+ 
+    cv2.putText(image, mytext, (100,300), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255) )
+ 
+    cv2.imshow('Black Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+ 
+ImageProcessing()
