46b7a0c3efa781dfefb49f51b30dbcc4351e7635
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-08 15:24:44 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,8 @@ else:
   select = st.selectbox('Equipment Subtype', df_asce_mep)
   select_df = df_asce_arch.loc[df_asce_mep['Mechanical and Electrical Components'] == select]
 ap = select_df['Ap']
-ap_print = st.latex('A_p') + ap
+ap_print = st.latex('A_p = \\ap',element, {
+  macros:  {
+    "\\ap": String(ap)
+  }
+});
