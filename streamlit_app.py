99b2d609efaf58c10329241e708e1f7fdec5f84b
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-08 16:19:22 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ select_df
 ap
 #checkbox for anchorage to concrete
 overstrength_true = st.checkbox('Anchorage to Concrete or Masonry? (Overstrength \u03A90)')
-if overstrength_true:
+#if overstrength_true:
   #st.write('A value of \u03A90 = ' + ap + ' will be used for anchorage') 
