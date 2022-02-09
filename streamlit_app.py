1cc4aeb4a771c1b272278182267875e47b84c4d6
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-09 09:57:35 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,4 @@ st.latex(r'''F_p = \frac{0.4 a_p S_{DS} W_p}{R_p/I_p} (1 +  2(\frac{z}{h}))''')
 def my_calc(p: float,q: float):
     d = p*q
     return locals()
-st.latex(my_calc(4,6))
+st.latex(my_calc())
