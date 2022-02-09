a7886a3b74d7c2c95250053ab5ca4d1d7945370b
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-08 16:35:23 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,14 +31,17 @@ else:
   select_df = df_asce_mep.loc[df_asce_mep['Mechanical and Electrical Components'] == select]
 try:
   ap = select_df['Ap'].values[0]
-  ap_print = st.latex('A_p')
+  rp = select_df['Rp'].values[0]
+  o0 = select_df['Omega-Not'].values[0]
 except:
   ap = select_df['Ap.1'].values[0]
+  rp = select_df['Rp.1'].values[0]
+  o0 = select_df['Omega-Not.1'].values[0]
 select_df
 #not quite done here.. how to display values nicely in latek with values inserted??
-ap
+
 #checkbox for anchorage to concrete
 overstrength_true = st.checkbox('Anchorage to Concrete or Masonry? (Overstrength \u03A90)')
 if overstrength_true:
-  ovr = "A value of \u03A90 = %d will be used for anchorage" % (ap) 
+  ovr = "A value of \u03A90 = %d will be used for anchorage" % (o0) 
   st.write(ovr)
