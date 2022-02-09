7d580cf6904962ea1fec83ec4573625ca662f7d1
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-08 17:26:10 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,4 @@ if overstrength_true:
   ovr = "A value of \u03A90 = %d will be used for anchorage" % (o0) 
   st.write(ovr)
 st.latex(r'''F_p = \frac{0.4 a_p S_{DS} W_p}{R_p/I_p} (1 +  2(\frac{z}{h}))''')
+st.latex(r'''F_p = \frac{0.4 %d S_{DS} W_p}{R_p/I_p} (1 +  2(\frac{z}{h}))''' % (ap))
