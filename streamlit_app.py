88239595724290492d63cdd7cc9f05956bff9e10
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-02-17 09:10:28 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,13 +51,13 @@ st.sidebar.write('test')
 select_equip = st.selectbox('Equipment Type', equip_sel)
 #if statement for types of equipment
 if select_equip == 'Architectural':
-  select = st.selectbox('Equipment Subtype (ASCE 7-16, Table 13.5-1)', df_asce_arch, disabled=b)
+  select = st.selectbox('Equipment Subtype (ASCE 7-16, Table 13.5-1)', df_asce_arch)
   select_df = df_asce_arch.loc[df_asce_arch['Architectural Components'] == select]
   ap = select_df['Ap'].values[0]
   rp = select_df['Rp'].values[0]
   o0 = select_df['Omega-Not'].values[0]
 else:
-  select = st.selectbox('Equipment Subtype (ASCE 7-16, Table 13.6-1)', df_asce_mep, disabled=b)
+  select = st.selectbox('Equipment Subtype (ASCE 7-16, Table 13.6-1)', df_asce_mep)
   select_df = df_asce_mep.loc[df_asce_mep['Mechanical and Electrical Components'] == select]
   ap = select_df['Ap.1'].values[0]
   rp = select_df['Rp.1'].values[0]
