e32c336bb711ba8807ab3cf8f9cedd881b54d91b
juanrossxo <77127202+juanrossxo@users.noreply.github.com>
2022-06-14 17:08:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,12 +119,12 @@ with st.expander('Fp Calculations',expanded=True):
   latex_code5, vals_dict = Fp_final_calc()
   st.latex(latex_code5)
 
-  #omega-not calc Fp
-  if overstrength_true:
+#omega-not calc Fp
+ if overstrength_true:
     with st.expander('\u03A90 Fp Calculations',expanded=True):
-      @handcalc()
-      def Fp_final_calc2():
-        F_p_final = Omega_0*min(max(F_p_1, F_p_2), F_p_3) #lb.
-        latex_code5, vals_dict = Fp_final_calc2()
-        st.latex(latex_code5)
+    @handcalc()
+    def Fp_final_calc2():
+      F_p_final = Omega_0*min(max(F_p_1, F_p_2), F_p_3) #lb.
+      latex_code5, vals_dict = Fp_final_calc2()
+      st.latex(latex_code5)
       
